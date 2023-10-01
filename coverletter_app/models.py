0cc17b64698b3f3a77a14f9de757eccd696059e1
viz1er <viz1er@yopmail.com>
2023-10-01 17:36:32 -0400

    add cover letter model

--- coverletter_app/models.py
+++ coverletter_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class CoverLetter(models.Model):
+    full_name = models.CharField(max_length=200)
+    job_description = models.TextField()
+    user_details = models.TextField()
+    generated_cover_letter = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
